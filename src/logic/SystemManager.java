79ecc2bb4a66debc9145f5a7c77394d3559c6f79
Jay <jtenorio51@unm.edu>
2024-04-25 15:13:06 -0600

    Added Lane creation

--- src/logic/SystemManager.java
+++ src/logic/SystemManager.java
@@ -1,35 +1,95 @@
 package logic;
 
+import javafx.stage.Screen;
+
 import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class SystemManager implements Runnable{
 
-    private static int numIntersection = 12;
+    private double size, xLane1, xLane2, xLane3, xLane4, xLane5, xLane6, xLane7, xLane8;
+    private double yLane1, yLane2, yLane3, yLane4, yLane5, yLane6, yLane7, yLane8, yLane9, yLane10,yLane11, yLane12;
+
+    private static int numIntersection = 6;
     protected static int sleepDelay = 10;
     private static int currentCars = 0;
     private static int maxNumCars = 100;
     protected static int createVehicleProbability = 20;
     protected static int createEMSProbability = 5;
+    private static String direction[] = {"North", "South", "East", "West"};
+    private double East_Lanes[];
+    private double West_Lanes[];
+    private double North_Lanes[];
 
-    private static float startingPOSX[] = new float[12];
-    private static float startingPOSY[] = new float[12];
+    private double South_Lanes[];
+
+    private double intersectionX_Coordinates[];
+    private double intersectionY_Coordinates[];
+
+    protected CopyOnWriteArrayList<String> testCarList = new CopyOnWriteArrayList<>();
+    protected CopyOnWriteArrayList<String> testIntersectionList = new CopyOnWriteArrayList<>();
+
+    //protected CopyOnWriteArrayList<Car> Cars = new CopyOnWriteArrayList<>();
+    //protected CopyOnWriteArrayList<Thread> CarThreads = new CopyOnWriteArrayList<>();
+    //protected CopyOnWriteArrayList<Intersection> intersections = new CopyOnWriteArrayList<>();
+    //protected CopyOnWriteArrayList<Thread> IntersectionThreads = new CopyOnWriteArrayList<>();
 
 
-    //protected List<Car> Cars = new ArrayList<Car>();
-    //protected List<Thread> Cars = new ArrayList<Thread>();
+    public SystemManager(int rows){
+         size = Screen.getPrimary().getBounds().getHeight() / (rows + 1);
+    }
 
-    //protected static Intersection intersections[];
-    //protected static Thread intersections[];
+    private void createLanes(){
+        //xLanes store y coordiantes. East-West Traffic.
+        xLane1 = size/2.85;
+        xLane2 = size/2.2;
+
+        xLane3 = size/1.825;
+        xLane4 = size/1.55;
+
+        xLane5 = (size*2)+(size/2.85);
+        xLane6 = (size*2)+(size/2.2);
+
+        xLane7 = (size*2)+(size/1.825);
+        xLane8 = (size*2)+(size/1.55);
 
 
+        //yLanes store x coordinates. North-South Traffic
+        yLane1 = size/2.85;
+        yLane2 = size/2.2;
+
+        yLane3 = size/1.825;
+        yLane4 = size/1.55;
+
+        yLane5 = (size*2)+(size/2.85);
+        yLane6 = (size*2)+(size/2.2);
+
+        yLane7 = (size*2)+(size/1.825);
+        yLane8 = (size*2)+(size/1.55);
+
+        yLane9 = (size*4)+(size/2.85);
+        yLane10 = (size*4)+(size/2.2);
+
+        yLane11 = (size*4)+(size/1.825);
+        yLane12 = (size*4)+(size/1.55);
+
+        East_Lanes = new double[]{xLane3, xLane4, xLane7, xLane8};
+        West_Lanes = new double[]{xLane1, xLane2, xLane5, xLane6};
+        North_Lanes = new double[]{yLane3, yLane4, yLane7, yLane8, yLane11, yLane12};
+        South_Lanes = new double[]{yLane1, yLane2, yLane5, yLane6, yLane9, yLane10};
+
+    }
+
+    private void createIntersection(){
+
+    }
 
     private void createVehicle(){
         //RNG EMS vehicle creation.
         //add to list
         currentCars++;
     }
-    private void createIntersection(){}
 
     private void createDMS(){}
 
@@ -61,6 +121,7 @@ public class SystemManager implements Runnable{
     @Override
     public void run() {
         int numCarCreate = 0;
+        createLanes();
         createIntersection();
         createDMS();
 
